4cb7c3f8b8bf494be6cfcde65a62002b38788279
Will Armijo <will.armijo@gmail.com>
2025-01-11 18:56:30 -0600

    domain_otx.py
    Updated the entire processing code

--- domain_otx.py
+++ domain_otx.py
@@ -6,6 +6,8 @@
 import requests
 import json
 
+print("This script is used to lookup reputational data for a Domain")
+
 domain = input("Please Enter the Domain name(example: google.com): ")
 
 url = f"https://otx.alienvault.com/api/v1/indicators/domain/{domain}/general"
@@ -14,11 +16,38 @@ response = requests.get(url)
 
 if response.status_code == 200:
     print("Successfully Accessed:", {url})
-    domain_info = response.json()
+    ip_info = response.json()
 
-    if domain_info["false_positive"]:
-        print("False Positive")
+    if ip_info["false_positive"]:
+        print("False Positive - Not  Malicious")
     else:
-        print(json.dumps(domain_info["pulse_info"], indent=0))
+        pulses = ip_info['pulse_info']['pulses']
+
+
+for items in pulses:
+   name = items['name']
+   descr = items['description']
+   tags = items['tags']
+   created = items['created']
+   modified = items['modified']
+
+print("==================")
+print("IoC Information: ")
+print("==================")
+print("")
+print("IoC Name: ", name)
+print("IoC Description: ", descr)
+print("IoC Created on: ", created)
+print("Last Updated", modified)
+print(tags)
+
+print("===============")
+print("Identified by: ")
+print("===============")
+print("")
+for items in pulses:
+    source_name = items['name']
+    print(source_name)
+
 else:
     print("Request failed with status", {response.status_code})
