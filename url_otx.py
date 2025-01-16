2284e4ddb58ff8536ec61f834604ae0353d4f7f0
Will Armijo <will.armijo@gmail.com>
2025-01-16 15:27:28 -0600

    Update url_otx.py

--- url_otx.py
+++ url_otx.py
@@ -6,52 +6,80 @@
 import requests
 import json
 
-ioc_url = input("Please Enter the Domain name(example: https://www.google.com): ")
+print("This script is used to lookup reputational data for a Domain")
 
-url = f"https://otx.alienvault.com/api/v1/indicators/url/{ioc_url}/general"
-print("Retreiving ", url)
-response = requests.get(url)
+url = input("Please Enter a Domain name: ")
 
-if response.status_code == 200:
-    print("Successfully Accessed:", {url})
-    ip_info = response.json()
+gen_url = f"https://otx.alienvault.com/api/v1/indicators/url/{url}/general"
+urls_url = f"https://otx.alienvault.com/api/v1/indicators/url/{url}/url_list"
 
-    if ip_info["false_positive"]:
+print("Retreiving Information Now...")
+gen_response = requests.get(gen_url)
+urls_response = requests.get(urls_url)
+
+if gen_response.status_code == 200:
+    gen_ip_info = gen_response.json()
+
+    if gen_ip_info['false_positive']:
         print("False Positive - Not  Malicious")
         exit()
-    
     else:
-        pulses = ip_info['pulse_info']['pulses']
-
-    #print(pulses)
+            pulses = gen_ip_info['pulse_info']['pulses']
+            related_pulses = gen_ip_info
     for items in pulses:
         name = items['name']
         descr = items['description']
-        trgtd_countries = items['targeted_countries']
         tags = items['tags']
         created = items['created']
         modified = items['modified']
-        references = items['references']
-    
-    #print(pulses)
-        print("")
-        print("==================")
-        print("IoC Information: ")
-        print("==================")
+        refs = items['references']
+        adversary = items['adversary']
+        trgt_countries = items['targeted_countries']
+
+        # Reporting Section
+        print("====================")
+        print("Related IoC Record: ")
+        print("====================")
         print("")
         print("IoC Name: ", name)
+        print("IOC Location Info: ")
+        print("Adversary: ", adversary)
         print("IoC Description: ", descr)
-        print("")
         print("IoC Created on: ", created)
-        print("Last Updated:", modified)
+        print("Last Updated", modified)
         print("")
-        print("Targeted Countries:")
-        for countries in trgtd_countries:
-            print(countries)
-        print("")
+        print("Related Tags: ")
+        #for tag in tags:
+            #print("\t", tag)
+        #print("")
         print("References:")
-        for refs in references:
-            print(refs)
+        for ref in refs:
+            print("\t", ref)
+        print("")
+    print("Targeted Countries: ")
+    for country in trgt_countries:
+        print("\t", country)
+    print("")
+    print("===============")
+    print("Identified by: ")
+    print("===============")
+    for items in pulses:
+        source_name = items['name']
+        print("\t", source_name)
+    print("")
 
+    if urls_response.status_code == 200:
+        urls_info = urls_response.json()
+    
+        urls_list = urls_info['url_list']
+        print("=============================")
+        print("****** Associated URLS ******")
+        print("=============================")
+        for listing in urls_list:
+            ass_urls = listing['url']
+            print("\t", ass_urls)
+            print("")
+    else:
+        print("Request failed with status", {urls_response.status_code}) # Error Code here
 else:
-    print("Request failed with status", {response.status_code})
+    print("Request failed with status", {gen_response.status_code}) # Error Code here
